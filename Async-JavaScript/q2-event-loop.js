0a9b73e1a6163444ffb60b4f1d415d8acc9de657
JAGDISH <js8724285@gmail.com>
2025-11-24 21:09:55 +0530

    [Jagdish] add. Task Scheduler: Micro vs Macro Challenge

---
+++ Async-JavaScript/q2-event-loop.js
@@ -0,0 +1,11 @@
+console.log("Start");
+
+setTimeout(() => {
+  console.log("setTimeout callback (macrotask)");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise.then callback (microtask)");
+});
+
+console.log("End");

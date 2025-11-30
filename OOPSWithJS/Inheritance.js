7364b234dc01ccea789958572588823ba0d04d0f
JAGDISH <js8724285@gmail.com>
2025-11-30 21:56:55 +0530

    [Jagdish] add .Employee Inheritance

---
+++ OOPSWithJS/Inheritance.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+    work() {
+        return `${this.name} works in ${this.department}`;
+    }
+}
+
+class Manager extends Employee {
+    work() {
+        return `${this.name} manages the ${this.department} department`;
+    }
+}
+
+const e1 = new Employee("Ramesh", "HR");
+const m1 = new Manager("Suresh", "IT");
+
+console.log(e1.work());
+console.log(m1.work());
